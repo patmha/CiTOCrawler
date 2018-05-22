12d8a61d96361e2d6b5a11666a79b20bffab25f8
patmha <patmha.dev@gmail.com>
2018-05-22 23:54:34 +0200

    Fixed dfs/bfs configuration and added comments

--- CiTOCrawler/CiTO_crawl.py
+++ CiTOCrawler/CiTO_crawl.py
@@ -8,8 +8,8 @@ import traceback
 import os
 
 
+# Define command-line arguments
 parser = argparse.ArgumentParser(description="Crawls the Web looking for pages with embedded RDF using the CiTO ontology")
-
 parser.add_argument("--conf", type=str, help="absolute path of configuration file")
 group1 = parser.add_mutually_exclusive_group(required=False)
 group1.add_argument("-s", "--seedpath", type=str, help="absolute path of file containing seed URLs")
@@ -21,21 +21,19 @@ parser.add_argument("-S", "--samedomain", action="store_true", help="search page
 parser.add_argument("-m", "--maxhours", type=int, help="max #hours for crawled pages before they get invalidated")
 parser.add_argument("-q", "--querypath", type=str, help="absolute path of file containing SPARQL query")
 parser.add_argument("-c", "--csvpath", type=str, help="absolute path of CSV with crawled pages and datetime of crawling")
-parser.add_argument("-D", "--dfs", action="store_true", help="force use of DFS (Depth-First Search) instead of visit order specified in configuration file")
-parser.add_argument("-V", "--visitonly", action="store_true", help="just visit webpages, without RDF parsing")
-parser.add_argument("--debugmode", action="store_true", help="debug mode")
+group2 = parser.add_mutually_exclusive_group(required=False)
+group2.add_argument("-B", "--bfs", action="store_true", help="force use of DBS (Breadth-First Search) instead of visit order specified in configuration file")
+group2.add_argument("-D", "--dfs", action="store_true", help="force use of DFS (Depth-First Search) instead of visit order specified in configuration file")
+parser.add_argument("-V", "--visitonly", action="store_true", help="just visit webpages, without RDF parsing; (for testing purposes)")
+parser.add_argument("--debug", action="store_true", help="use debug mode (which generates extra output)")
 
-# group2 = parser.add_mutually_exclusive_group(required=False)
-# group2.add_argument("-B", "--bfs", action="store_true", help="use breadth-first search")
-# group2.add_argument("-D", "--dfs", action="store_true", help="use depth-first search")
-
+# Parse command-line arguments
 args = parser.parse_args()#['-sseeds.txt', '--bfs'])
 print "In {0}. Args passed in:\n".format(os.path.basename(__file__)), vars(args)
 
-# default configuration file is called "conf.json" and is located in the same directory containing this module.
+# Get full path of the default configuration file "conf.json" (located in the same directory containing this module)
 conf_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'conf.json'))
 if args.conf:
-    # TODO: accept a relative (to the directory containing this module) path as well
     conf_path = args.conf
 
 
@@ -45,19 +43,15 @@ try:
     with open(conf_path, "r") as fd:
         file_content = fd.read()
     conf_dict = json.loads(file_content)
+    # TODO: check keys in JSON configuration file.
 except Exception:
     print "\n"
     traceback.print_exc(file=sys.stdout)
     sys.exit(1)
 print "\nIn {0}. conf_dict as loaded from JSON configuration file:\n".format(os.path.basename(__file__)), conf_dict
 
-# for k,v in conf_dict.items():
-#     print k, v
-
-
 # Update conf_dict with command line values
 if args.seedpath:
-    # TODO: accept a relative (to the directory containing this module) path as well
     conf_dict['seedpath'] = args.seedpath
 elif args.urllist:
     conf_dict['urllist'] = args.urllist
@@ -70,39 +64,29 @@ if args.samedomain:
 if args.maxhours:
     conf_dict['maxhours'] = args.maxhours
 if args.querypath:
-    # TODO: accept a relative (to the directory containing this module) path as well
     conf_dict['querypath'] = args.querypath
 if args.csvpath:
-    # TODO: accept a relative (to the directory containing this module) path as well
     conf_dict['csvpath'] = args.csvpath
 if args.depth:
     conf_dict['depth'] = args.depth
-if args.dfs:  # use DFS. if not set, visit order will be the one specified in the JSON configuration file (conf_path)
+if args.bfs:
+    conf_dict['bfs'] = True
+elif args.dfs:
     conf_dict['bfs'] = False
-# else:  # args.dfs is not set, therefore BFS is implied
-#     conf_dict['bfs'] = True
+# if a visit order flag was not set, visit order will be BFS or DFS, depending on the value for key "bfs" in conf_dict
+
 if args.visitonly:
     conf_dict['visitonly'] = True
-
-# if args.bfs:  # use BFS
-#     conf_dict['bfs'] = True
-# else:  # args.dfs is set
-#     conf_dict['bfs'] = False
-
-# nuconfjson = json.dumps(conf_dict)
-# with open("nuconf.json", "w") as f:
-#     f.write(nuconfjson)
+if args.debug:
+    conf_dict['debug'] = True
 
 print "\nIn {0}. conf_dict after update with cmd line args:\n".format(os.path.basename(__file__)), conf_dict
 
-settings = get_project_settings()
-# print "\n\nIn CiTO_crawl.py ... project settings BEFORE updating with cmdline args are:"
-# for s, v in settings.items():
-#     print("{0} = {1}".format(s,v))
-# s = settings.get("DEFAULT_REQUEST_HEADERS")
-# for k, v in s.items():
-#     print(k, v)
 
+# get Scrapy default project settings (i.e. those in settings.py)
+settings = get_project_settings()  # returns a Settings object
+
+# update default project settings with conf_dict values
 if conf_dict.get('depth') is not None:
     settings.set('DEPTH_LIMIT', conf_dict['depth'])
 else:
@@ -114,21 +98,18 @@ if conf_dict['bfs'] is True:
     settings.set('SCHEDULER_MEMORY_QUEUE', 'scrapy.squeues.FifoMemoryQueue')
 else:  # conf.bfs is False, which means DFS should be used
     settings.set('DEPTH_PRIORITY', -100)
-    # default settings, so no need to update them
+    # these are the default settings, so no need to update them
     # SCHEDULER_DISK_QUEUE = 'scrapy.squeues.PickleLifoDiskQueue'
     # SCHEDULER_MEMORY_QUEUE = 'scrapy.squeues.LifoMemoryQueue'
 
-# print "\n\n\n\nIn CiTO_crawl.py ... project settings AFTER updating with cmdline args are:"
-# for s, v in settings.items():
-#     print("{0} = {1}".format(s,v))
-# s = settings.get("DEFAULT_REQUEST_HEADERS")
-# for k, v in s.items():
-#     print(k, v)
 
-# settings.set("LOG_FILE", "TEST.LOG")
-
-
+# Create CrawlerProcess object with scrapy.settings.Settings instance passed in
 process = CrawlerProcess(settings)
+# Run crawler by creating Crawler object for spider class associated with spider name and by calling its crawl method
+# which then instantiates a CitoSpider object with **conf_dict as arguments,
+# "while setting the execution engine in motion.
+# Returns a deferred that is fired when the crawl is finished." (quoted from Scrapy documentation)
 process.crawl('cito', **conf_dict)
+# Start Twisted reactor
 process.start()  # the script will block here until the crawling is finished
 print("\n\n... Done.")
