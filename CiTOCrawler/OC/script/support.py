726d1dd009b843c644f82955b71bcb20bf3b4ed2
patmha <patmha.dev@gmail.com>
2018-05-29 14:42:48 +0200

    Commented out functions and imports that are not needed

--- CiTOCrawler/OC/script/support.py
+++ CiTOCrawler/OC/script/support.py
@@ -14,8 +14,8 @@
 # ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
 # SOFTWARE.
 
-from requests.exceptions import ReadTimeout, ConnectTimeout
-import json
+# from requests.exceptions import ReadTimeout, ConnectTimeout
+# import json
 
 __author__ = 'essepuntato'
 
@@ -23,10 +23,10 @@ import unicodedata
 import re
 import os
 import shutil
-from nltk.metrics import binary_distance as lev
+# from nltk.metrics import binary_distance as lev
 from rdflib import Literal, RDF
-from time import sleep
-import requests
+# from time import sleep
+# import requests
 from urllib import quote
 import sys
 
@@ -98,31 +98,31 @@ def list_from_idx(l, idx_l):
 
     return result
 
-
-def string_list_close_match(ls, m):
-    final_result = []
-
-    tmp_result = []
-    m_ascii = normalise_name(m)
-    f_letter = m_ascii[:1]
-    for idx, s in enumerate(ls):
-        if normalise_name(s)[:1] == f_letter:
-            tmp_result += [idx]
-
-    if tmp_result == 1:
-        final_result = tmp_result
-    elif tmp_result > 1:
-        cur_lev = 10000000
-        for idx in tmp_result:
-            s = ls[idx]
-            s_lev = lev(normalise_name(s), m_ascii)
-            if s_lev <= cur_lev:
-                if s_lev < cur_lev:
-                    final_result = []
-                final_result += [idx]
-                cur_lev = s_lev
-
-    return final_result
+#
+# def string_list_close_match(ls, m):
+#     final_result = []
+#
+#     tmp_result = []
+#     m_ascii = normalise_name(m)
+#     f_letter = m_ascii[:1]
+#     for idx, s in enumerate(ls):
+#         if normalise_name(s)[:1] == f_letter:
+#             tmp_result += [idx]
+#
+#     if tmp_result == 1:
+#         final_result = tmp_result
+#     elif tmp_result > 1:
+#         cur_lev = 10000000
+#         for idx in tmp_result:
+#             s = ls[idx]
+#             s_lev = lev(normalise_name(s), m_ascii)
+#             if s_lev <= cur_lev:
+#                 if s_lev < cur_lev:
+#                     final_result = []
+#                 final_result += [idx]
+#                 cur_lev = s_lev
+#
+#     return final_result
 
 
 def move_file(src, dst):
@@ -158,57 +158,57 @@ def get_short_name(res):
 def get_count(res):
     return re.sub("^.+/[a-z][a-z]/([0-9]+)$", "\\1", str(res))
 
-
-def get_data(max_iteration, sec_to_wait, get_url, headers, timeout, repok, reper, is_json=True):
-    tentative = 0
-    error_no_200 = False
-    error_read = False
-    error_connection = False
-    error_generic = False
-    errors = []
-    while tentative < max_iteration:
-        if tentative != 0:
-            sleep(sec_to_wait)
-        tentative += 1
-
-        try:
-            response = requests.get(get_url, headers=headers, timeout=timeout)
-            if response.status_code == 200:
-                repok.add_sentence("Data retrieved from '%s'." % get_url)
-                if is_json:
-                    return json.loads(response.text)
-                else:
-                    return response.text
-            else:
-                err_string = "We got an HTTP error when retrieving data (HTTP status code: %s)." % \
-                             str(response.status_code)
-                if not error_no_200:
-                    error_no_200 = True
-                if response.status_code == 404:
-                    repok.add_sentence(err_string + " However, the process could continue anyway.")
-                    # If the resource has not found, we can break the process immediately,
-                    # by returning None so as to allow the callee to continue (or not) the process
-                    return None
-                else:
-                    errors += [err_string]
-        except ReadTimeout as e:
-            if not error_read:
-                error_read = True
-                errors += ["A timeout error happened when reading results from the API "
-                           "when retrieving data. %s" % e]
-        except ConnectTimeout as e:
-            if not error_connection:
-                error_connection = True
-                errors += ["A timeout error happened when connecting to the API "
-                           "when retrieving data. %s" % e]
-        except Exception as e:
-            if not error_generic:
-                error_generic = True
-                errors += ["A generic error happened when trying to use the API "
-                           "when retrieving data. %s" % sys.exc_info()[0]]
-
-    # If the process comes here, no valid result has been returned
-    reper.add_sentence(" | ".join(errors) + "\n\tRequested URL: " + get_url)
+#
+# def get_data(max_iteration, sec_to_wait, get_url, headers, timeout, repok, reper, is_json=True):
+#     tentative = 0
+#     error_no_200 = False
+#     error_read = False
+#     error_connection = False
+#     error_generic = False
+#     errors = []
+#     while tentative < max_iteration:
+#         if tentative != 0:
+#             sleep(sec_to_wait)
+#         tentative += 1
+#
+#         try:
+#             response = requests.get(get_url, headers=headers, timeout=timeout)
+#             if response.status_code == 200:
+#                 repok.add_sentence("Data retrieved from '%s'." % get_url)
+#                 if is_json:
+#                     return json.loads(response.text)
+#                 else:
+#                     return response.text
+#             else:
+#                 err_string = "We got an HTTP error when retrieving data (HTTP status code: %s)." % \
+#                              str(response.status_code)
+#                 if not error_no_200:
+#                     error_no_200 = True
+#                 if response.status_code == 404:
+#                     repok.add_sentence(err_string + " However, the process could continue anyway.")
+#                     # If the resource has not found, we can break the process immediately,
+#                     # by returning None so as to allow the callee to continue (or not) the process
+#                     return None
+#                 else:
+#                     errors += [err_string]
+#         except ReadTimeout as e:
+#             if not error_read:
+#                 error_read = True
+#                 errors += ["A timeout error happened when reading results from the API "
+#                            "when retrieving data. %s" % e]
+#         except ConnectTimeout as e:
+#             if not error_connection:
+#                 error_connection = True
+#                 errors += ["A timeout error happened when connecting to the API "
+#                            "when retrieving data. %s" % e]
+#         except Exception as e:
+#             if not error_generic:
+#                 error_generic = True
+#                 errors += ["A generic error happened when trying to use the API "
+#                            "when retrieving data. %s" % sys.exc_info()[0]]
+#
+#     # If the process comes here, no valid result has been returned
+#     reper.add_sentence(" | ".join(errors) + "\n\tRequested URL: " + get_url)
 
 
 def is_dataset(string_iri):
